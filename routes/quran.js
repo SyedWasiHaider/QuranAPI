20211c0b3bb6788123fdd67389390379a642cabe
WasiDeveloper <syedwasihaider@github.com>
2014-12-06 22:54:16 -0500

    Added Arabic and few more functions

--- routes/quran.js
+++ routes/quran.js
@@ -6,29 +6,56 @@ var Server = mongo.Server,
 Db = mongo.Db,
 BSON = mongo.BSONPure;
 
-var collectionName = "VerseCollection";
+/*********SOME CONSTANTS****************/
+var englishCollectionName = "englishCollectionName";
+var quranArabicCollectionName = "quranArabicCollectionName";
+var maxVerses = 6236
 var dbName = "QuranDatabase";
 
 var server = new Server('localhost', 27017, {auto_reconnect: true});
 db = new Db(dbName, server, {w:1});
 
+
+//Opens a connection to the database and checks to see if our collecions exists
+//and creates them if necessary.
 db.open(function(err, db) {
 	console.log(err)
 	if(!err) {
 		console.log("Connected to database");
-		db.collection(collectionName, {strict:true}, function(err, collection) {
+		db.collection(englishCollectionName, {strict:true}, function(err, collection) {
 			if (err) {
 				console.log("The 'verses' collection doesn't exist. Creating now...");
-				populateDB();
+				populateTranslation();
 			}
 		});
+
+		db.collection(quranArabicCollectionName, {strict:true}, function(err, collection) {
+			if (err) {
+				console.log("The 'quran Arabic' collection doesn't exist. Creating now...");
+				populateQuran();
+			}
+		});
+
 	}
 }); 
 
+/*********************THIS PROVIDES THE MAIN FUNCTIONALITY OF THE API*********************/
+
+function getCollectionNameFromLanguage(language){
+	var collectionToUse = quranArabicCollectionName;
+	if (language){
+		
+		if (language.toUpperCase() === "ENGLISH"){
+			collectionToUse = englishCollectionName;
+		}
+	}
+	return collectionToUse;
+} 
+
 
 exports.findAll = function(req, res) {
-	console.log("findAll");
-	db.collection(collectionName, function(err, collection) {
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
 		collection.find().toArray(function(err, items) {
 			res.send(items);
 		});
@@ -36,7 +63,8 @@ exports.findAll = function(req, res) {
 }; 
 
 exports.findByIndex = function(req, res) {
-	db.collection(collectionName, function(err, collection) {
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
 		var _index =  parseInt(req.params.id);
 		console.log(_index);
 		collection.find({index: _index}).toArray(function(err, items) {
@@ -45,8 +73,21 @@ exports.findByIndex = function(req, res) {
 	});
 }; 
 
+
+exports.findBySurah = function (req, res){
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
+		var _surah = parseInt(req.params.surah);
+		collection.find({surah: _surah}).toArray(function(err, items) {
+			res.send(items);
+		});
+	});
+}
+
+
 exports.findBySurahVerse = function (req, res){
-	db.collection(collectionName, function(err, collection) {
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
 		var _verse =  parseInt(req.params.verse);
 		var _surah = parseInt(req.params.surah);
 		console.log(_surah + ":" + _verse);
@@ -58,7 +99,8 @@ exports.findBySurahVerse = function (req, res){
 
 
 exports.findByIndexRange = function(req, res) {
-	db.collection(collectionName, function(err, collection) {
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
 		var _indexFrom =  parseInt(req.params.from);
 		var _indexTo = parseInt(req.params.to);
 		collection.find({index: {$gte: _indexFrom, $lte: _indexTo}}).toArray(function(err, items) {
@@ -69,7 +111,8 @@ exports.findByIndexRange = function(req, res) {
 
 
 exports.findBySearchTerm = function(req, res) {
-	db.collection(collectionName, function(err, collection) {
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
 		var _text = req.params.term;
 		console.log(_text);
 		collection.find({text : {$regex : ".*" + _text+ ".*", $options: "i"}}).toArray(function(err, items) {
@@ -78,14 +121,27 @@ exports.findBySearchTerm = function(req, res) {
 	});
 }; 
 
+exports.findRandomVerse = function (req, res){
+	var collectionToUse = getCollectionNameFromLanguage(req.params.language); 
+	db.collection(collectionToUse, function(err, collection) {
+		var _index =  Math.floor((Math.random() * maxVerses));
+		console.log("index calculated was"+_index);
+		collection.find({index: _index}).toArray(function(err, items) {
+			res.send(items);
+		});
+	});
+}
+
+
+
 
 
 //-------------THIS SHOULD ONLY BE REALLY CALLED ONCE --------/
 //TODO: In fact it might better to just take it out once the database has been populated.
 
-var populateDB = function() {
+var populateTranslation = function() {
 
-	console.log("populateDB")
+	console.log("populateTranslation")
 
 	fs.readFile('./assets/en.ahmedali.txt', 'utf8', function (err,data) {
 		if (err) {
@@ -94,7 +150,7 @@ var populateDB = function() {
 
 		var lines = data.split('\n');
 
-		db.collection(collectionName, function(err, collection) {
+		db.collection(englishCollectionName, function(err, collection) {
 			_.each(lines, function(row, idx){
 
 				var members = row.split('|');
@@ -103,6 +159,33 @@ var populateDB = function() {
 				var _text = members[2];
 				console.log(members);
 				collection.insert({index: idx, surah: _surah, verse : _verse, text : _text}, {safe:true}, function(err, result) {});
+			})
+		});
+	});
+}; 
+
+
+var populateQuran = function() {
+
+	console.log("populateQuran")
+
+	fs.readFile('./assets/quran-simple-enhanced.txt', 'utf8', function (err,data) {
+		if (err) {
+			return console.log(err);
+		}
+
+		var lines = data.split('\n');
+
+		db.collection(quranArabicCollectionName, function(err, collection) {
+			_.each(lines, function(row, idx){
+				if (idx < maxVerses){
+					var members = row.split('|');
+					var _surah = parseInt(members[0]);
+					var _verse = parseInt(members[1]);
+					var _text = members[2];
+					console.log(members);
+					collection.insert({index: idx, surah: _surah, verse : _verse, text : _text}, {safe:true}, function(err, result) {});
+				}
 			})
 		});
 	});
--- server.js
+++ server.js
@@ -2,12 +2,15 @@ var express = require('express');
 var app = express();
 verses = require('./routes/quran');
 
-app.get('/quran', verses.findAll);
-app.get('/quran/byIndex/:id', verses.findByIndex);
-app.get('/quran/bySurahVerse/:surah/:verse', verses.findBySurahVerse)
-app.get('/quran/byRange/:from/:to', verses.findByIndexRange);
-app.get('/quran/bySearchTerm/:term', verses.findBySearchTerm)
 
+//This sets up the routing for the Arabic.
+app.get('/quran/all/:language?', verses.findAll);
+app.get('/quran/byIndex/:language?/:id', verses.findByIndex);
+app.get('/quran/bySurah/:language?/:surah', verses.findBySurah)
+app.get('/quran/bySurahVerse/:language?/:surah/:verse', verses.findBySurahVerse)
+app.get('/quran/byRange/:language?/:from/:to', verses.findByIndexRange);
+app.get('/quran/bySearchTerm/:language?/:term', verses.findBySearchTerm);
+app.get('/quran/randomVerse/:language?', verses.findRandomVerse);
 
 app.listen(3000);
 console.log('Listening on port 3000...'); 
